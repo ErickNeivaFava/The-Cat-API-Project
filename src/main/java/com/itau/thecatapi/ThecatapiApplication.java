2850a6dbaf843c74ce486872cf16af3543566f5e
erick.fava <erickneivafava@gmail.com>
2025-09-19 14:12:28 -0300

    chore: preparando properties e rename

--- src/main/java/com/itau/thecatapi/ThecatapiApplication.java
+++ src/main/java/com/itau/thecatapi/ThecatapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThecatapiApplication {
+public class TheCatAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThecatapiApplication.class, args);
+		SpringApplication.run(TheCatAPIApplication.class, args);
 	}
 
 }
