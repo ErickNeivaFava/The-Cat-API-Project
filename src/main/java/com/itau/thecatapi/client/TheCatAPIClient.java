1ffa10c801746aa835de4480c2eff54b13f6ed64
erick.fava <erickneivafava@gmail.com>
2025-09-21 14:27:45 -0300

    feat: adicionando metodo para buscar categorias e buscar por criteria (oculos/chapeu)

--- src/main/java/com/itau/thecatapi/client/TheCatAPIClient.java
+++ src/main/java/com/itau/thecatapi/client/TheCatAPIClient.java
@@ -1,14 +1,12 @@
 package com.itau.thecatapi.client;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.itau.thecatapi.dto.BreedImageDTO;
 import com.itau.thecatapi.model.Breed;
 import com.itau.thecatapi.model.BreedImage;
+import com.itau.thecatapi.model.Category;
+import com.itau.thecatapi.service.CategoryService;
 import com.itau.thecatapi.utils.HttpUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -31,17 +29,19 @@ public class TheCatAPIClient {
 //    @Value("${thecatapi.api-key}")
 //    private String apiKey;
 
+    private CategoryService categoryService;
+
     private final HttpUtils httpUtils;
 
     private final RestTemplate restTemplate;
 
     private final ExecutorService apiExecutor = Executors.newFixedThreadPool(10);
 
-    public TheCatAPIClient(HttpUtils httpUtils, RestTemplate restTemplate) {
+    public TheCatAPIClient(CategoryService categoryService, HttpUtils httpUtils, RestTemplate restTemplate) {
+        this.categoryService = categoryService;
         this.httpUtils = httpUtils;
         this.restTemplate = restTemplate;
     }
-
 
     public CompletableFuture<List<Breed>> getAllBreedsAsync() {
         return CompletableFuture.supplyAsync(() -> {
@@ -120,8 +120,11 @@ public class TheCatAPIClient {
 
             List<List<String>> batches = new ArrayList<>();
 
-            for (int i = 0; i < breedIds.size(); i += 10) {
-                int end = Math.min(breedIds.size(), i + 10);
+            int batchSize = 20; //TODO: adicionar verificação de api key para definir tamanho do batch
+            //TODO: hats/sunglasses precisa ter a verificação também
+
+            for (int i = 0; i < breedIds.size(); i += batchSize) {
+                int end = Math.min(breedIds.size(), i + batchSize);
                 batches.add(breedIds.subList(i, end));
             }
 
@@ -132,7 +135,7 @@ public class TheCatAPIClient {
                     String breedIdsParam = String.join(",", batch);
 
                     String url = UriComponentsBuilder.fromHttpUrl(httpUtils.buildUrl("/images/search"))
-                            .queryParam("limit", 3)
+                            .queryParam("limit", 3 * batch.size())
                             .queryParam("breed_ids", breedIdsParam)
                             .toUriString();
 
@@ -161,6 +164,78 @@ public class TheCatAPIClient {
                     System.err.println("Erro ao processar batch: " + batch + " - " + e.getMessage());
                 }
             }
+            return breedImages;
+        }, apiExecutor);
+
+    }
+
+    public CompletableFuture<List<Category>> getCategoriesAsync() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<Category> categories = new ArrayList<>();
+
+            ObjectMapper objectMapper = httpUtils.getObjectMapper();
+
+            //category 1 = hats, 4 = sunglasses
+            try {
+
+
+                String url = UriComponentsBuilder.fromHttpUrl(httpUtils.buildUrl("/categories"))
+                        .toUriString();
+
+                ResponseEntity<String> response = restTemplate.exchange(
+                        url, HttpMethod.GET, httpUtils.createDefaultEntity(), String.class);
+
+                categories = objectMapper.readValue(
+                        response.getBody(),
+                        objectMapper.getTypeFactory().constructCollectionType(List.class, Category.class));
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao mapear resposta JSON", e);
+            }
+
+            return categories;
+        }, apiExecutor);
+    }
+
+    public CompletableFuture<List<BreedImage>> getBreedImagesByCriteriaAsync(List<String> criteria) {
+        return CompletableFuture.supplyAsync(() -> {
+            List<BreedImage> breedImages = new ArrayList<>();
+
+            ObjectMapper objectMapper = httpUtils.getObjectMapper();
+
+            List<String> categoryIds = categoryService.getCategoriesByNames(criteria).stream()
+                    .map(category -> String.valueOf(category.getId()))
+                    .collect(Collectors.toList());;
+
+            //category 1 = hats, 4 = sunglasses
+            try {
+                String categoryIdsParam = String.join(",", categoryIds);
+
+                String url = UriComponentsBuilder.fromHttpUrl(httpUtils.buildUrl("/images/search"))
+                        .queryParam("limit", 3)
+                        .queryParam("category_ids", categoryIdsParam)
+                        .toUriString();
+
+                ResponseEntity<String> response = restTemplate.exchange(
+                        url, HttpMethod.GET, httpUtils.createDefaultEntity(), String.class);
+
+                List<BreedImageDTO> responses = objectMapper.readValue(
+                        response.getBody(),
+                        objectMapper.getTypeFactory().constructCollectionType(List.class, BreedImageDTO.class));
+
+                List<BreedImage> batchImages = responses.stream()
+                        .map(BreedImage::fromResponse)
+                        .collect(Collectors.toList());
+
+                if (batchImages != null && !batchImages.isEmpty()) {
+                    breedImages.addAll(batchImages);
+                }
+
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao mapear resposta JSON", e);
+            }
+
             return breedImages;
         }, apiExecutor);
 
