8f5829a9dec390f172bb369ced3f61d0f1f3b4b0
erick.fava <erickneivafava@gmail.com>
2025-09-22 21:56:19 -0300

    feat: image repository

--- src/main/java/com/itau/thecatapi/repository/BreedImageRepository.java
+++ src/main/java/com/itau/thecatapi/repository/BreedImageRepository.java
@@ -4,6 +4,16 @@ import com.itau.thecatapi.model.BreedImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BreedImageRepository extends JpaRepository<BreedImage, String> {
+
+    List<BreedImage> findByBreedId(String breedId);
+
+    List<BreedImage> findByCategoryId(Integer categoryId);
+
+    List<BreedImage> findByBreedIdAndCategoryId(String breedId, Integer categoryId);
+
+    List<BreedImage> findByFavouriteTrue();
 }
